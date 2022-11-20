77bb1665bbe57571f21c3ac1d173113cb83fe80e
Dwane Debono <dwane.debono@gmail.com>
2022-11-20 13:25:48 +0000

    Updating Cucumber runner

--- src/test/java/com/maxprofit/calculator/RunCucumberTest.java
+++ src/test/java/com/maxprofit/calculator/RunCucumberTest.java
@@ -1,9 +1,17 @@
 package com.maxprofit.calculator;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(glue = {"com.maxprofit.calculator"})
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/maxprofit/calculator")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.maxprofit.calculator")
 public class RunCucumberTest {
 }
