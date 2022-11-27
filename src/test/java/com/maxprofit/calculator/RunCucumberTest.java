e7d504c863cb7488e99efb593c0735491b882b28
Dwane Debono <dwane.debono@gmail.com>
2022-11-27 19:10:24 +0000

    Fix

--- src/test/java/com/maxprofit/calculator/RunCucumberTest.java
+++ src/test/java/com/maxprofit/calculator/RunCucumberTest.java
@@ -12,8 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/maxprofit/calculator")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report/report.json")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-report/report.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report/report.json, html:target/cucumber-report/report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.maxprofit.calculator")
 public class RunCucumberTest {
 }
