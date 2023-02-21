6788754bd8a5db092d61e79ce6576c0de1bc72e7
Dwane Debono <dwane.debono@gmail.com>
2023-02-21 08:19:33 +0000

    linting

--- src/test/java/com/maxprofit/calculator/steps/StepDefinitions.java
+++ src/test/java/com/maxprofit/calculator/steps/StepDefinitions.java
@@ -23,136 +23,148 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StepDefinitions {
 
-    static Logger logger = Logger.getLogger(Stock.class.getName());
+        static Logger logger = Logger.getLogger(Stock.class.getName());
 
-    int savingsContext = 0;
-    List<Integer> currentPricesContext;
-    List<Integer> futurePricesContext;
+        /*
+         * Integer storing the amount of savings.
+         */
+        int savingsContext = 0;
 
-    /**
-     * Sets the value of savingsContext to the given savings.
-     *
-     * @param savings an integer representing the amount of savings.
-     */
-    @Given("I have {int} Euros of savings")
-    public void IHaveEurosOfSavings(int savings) {
+        /*
+         * List storing the current prices.
+         */
+        List<Integer> currentPricesContext;
 
-        System.out.format("Savings: %d\n", savings);
-        savingsContext = savings;
-    }
+        /*
+         * List storing the future prices.
+         */
+        List<Integer> futurePricesContext;
 
-    /**
-     * Sets the value of currentPricesContext to a List of integers parsed from the
-     * given string.
-     *
-     * @param currentPrices a string representing a comma-separated list
-     *                      of current stock prices.
-     */
-    @When("Array of current stock prices are {string}")
-    public void arrayOfCurrentStockPricesAre(final String currentPrices) {
-        System.out.format("Current prices: %s\n", currentPrices);
-        currentPricesContext = Stream.of(currentPrices.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
+        /**
+         * Sets the value of savingsContext to the given savings.
+         *
+         * @param savings an integer representing the amount of savings.
+         */
+        @Given("I have {int} Euros of savings")
+        public void IHaveEurosOfSavings(int savings) {
 
-    /**
-     * Compares the expected result to the actual result of the
-     * calculation for the
-     * best combination of indices for maximum profit.
-     *
-     * @param result a string representing the expected result.
-     */
-    @Then("the best combination of indices for max profit is {string}")
-    public void theBestCombinationOfIndicesForMaxProfitIs(final String result) {
-        final List<Integer> resultIndices = Stream.of(result.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        final CalculationResult actualResult = Stock.returnIndicesMaxProfit(
-                savingsContext, currentPricesContext,
-                futurePricesContext);
-
-        if (actualResult.indices.size() == 1) {
-            assertEquals(resultIndices, actualResult.indices.get(0),
-                    "Actual Result: " + actualResult.indices + " with profit of "
-                            + actualResult.maxProfit);
-        } else {
-            logger.log(Level.WARNING,
-                    String.format("Multiple combinations with same used savings: "
-                            + actualResult.indices));
-            assertTrue(actualResult.indices.contains(resultIndices),
-                    "Actual Result: " + actualResult.indices + " with profit of "
-                            + actualResult.maxProfit);
+                System.out.format("Savings: %d\n", savings);
+                savingsContext = savings;
         }
-    }
 
-    /**
-     * Sets the value of futurePricesContext to a List of integers parsed from the
-     * given string.
-     *
-     * @param futurePrices a string representing a comma-separated list of future
-     *                     stock prices.
-     */
-    @And("Array of future stock prices are {string}")
-    public void arrayOfFutureStockPricesAre(final String futurePrices) {
-        System.out.format("Future prices: %s\n", futurePrices);
-        futurePricesContext = Stream.of(futurePrices.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
+        /**
+         * Sets the value of currentPricesContext to a List of integers parsed from the
+         * given string.
+         *
+         * @param currentPrices a string representing a comma-separated list
+         *                      of current stock prices.
+         */
+        @When("Array of current stock prices are {string}")
+        public void arrayOfCurrentStockPricesAre(final String currentPrices) {
+                System.out.format("Current prices: %s\n", currentPrices);
+                currentPricesContext = Stream.of(currentPrices.split(","))
+                                .map(String::trim)
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList());
+        }
 
-    /**
-     * Compares the expected profit to the actual profit of the calculation.
-     *
-     * @param profit an integer representing the expected profit.
-     */
-    @Then("profit is {int} Euros")
-    public void profitIsEuros(final int profit) {
-        System.out.format("Profit: %d\n", profit);
-        int actualProfit = Stock.returnIndicesMaxProfit(savingsContext,
-                currentPricesContext,
-                futurePricesContext).maxProfit;
-        assertEquals(profit, actualProfit, "Actual Profit is %s " + actualProfit);
-    }
+        /**
+         * Compares the expected result to the actual result of the
+         * calculation for the
+         * best combination of indices for maximum profit.
+         *
+         * @param result a string representing the expected result.
+         */
+        @Then("the best combination of indices for max profit is {string}")
+        public void theBestCombinationOfIndicesForMaxProfitIs(final String result) {
+                final List<Integer> resultIndices = Stream.of(result.split(","))
+                                .map(String::trim)
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList());
 
-    /**
-     * Asserts that there is no set of indices with a profit.
-     *
-     */
-    @Then("there is no best combination for max profit")
-    public void thereIsNoBestCombinationForMaxProfit() {
-        final CalculationResult actualResult = Stock.returnIndicesMaxProfit(
-                savingsContext,
-                currentPricesContext,
-                futurePricesContext);
-        assertEquals(0, actualResult.indices.size(), "Actual Result: "
-                + actualResult.indices);
-    }
+                final CalculationResult actualResult = Stock.returnIndicesMaxProfit(
+                                savingsContext, currentPricesContext,
+                                futurePricesContext);
 
-    /**
-     * Asserts that profit is 0.
-     *
-     */
-    @And("no profit is made")
-    public void noProfitIsMade() {
-        final int actualProfit = Stock.returnIndicesMaxProfit(savingsContext,
-                currentPricesContext,
-                futurePricesContext).maxProfit;
-        assertEquals(0, actualProfit, "Actual Profit is %s " + actualProfit);
-    }
+                if (actualResult.indices.size() == 1) {
+                        assertEquals(resultIndices, actualResult.indices.get(0),
+                                        "Actual Result: " + actualResult.indices + " with profit of "
+                                                        + actualResult.maxProfit);
+                } else {
+                        logger.log(Level.WARNING,
+                                        String.format("Multiple combinations with same used savings: "
+                                                        + actualResult.indices));
+                        assertTrue(actualResult.indices.contains(resultIndices),
+                                        "Actual Result: " + actualResult.indices + " with profit of "
+                                                        + actualResult.maxProfit);
+                }
+        }
 
-    /**
-     * Asserts that with the given indices, same profit is achieved as previously
-     * stored.
-     *
-     * @param indices combination representing the indices of stocks.
-     */
-    @And("same savings and max profit is achieved with the indices {string}")
-    public void sameSavingsMaxProfitIsWithCombination(final String indices) {
-        this.theBestCombinationOfIndicesForMaxProfitIs(indices);
-    }
+        /**
+         * Sets the value of futurePricesContext to a List of integers parsed from the
+         * given string.
+         *
+         * @param futurePrices a string representing a comma-separated list of
+         *                     future stock prices.
+         */
+        @And("Array of future stock prices are {string}")
+        public void arrayOfFutureStockPricesAre(final String futurePrices) {
+                System.out.format("Future prices: %s\n", futurePrices);
+                futurePricesContext = Stream.of(futurePrices.split(","))
+                                .map(String::trim)
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList());
+        }
+
+        /**
+         * Compares the expected profit to the actual profit of the calculation.
+         *
+         * @param profit an integer representing the expected profit.
+         */
+        @Then("profit is {int} Euros")
+        public void profitIsEuros(final int profit) {
+                System.out.format("Profit: %d\n", profit);
+                int actualProfit = Stock.returnIndicesMaxProfit(savingsContext,
+                                currentPricesContext,
+                                futurePricesContext).maxProfit;
+                assertEquals(profit, actualProfit, "Actual Profit is %s "
+                                + actualProfit);
+        }
+
+        /**
+         * Asserts that there is no set of indices with a profit.
+         *
+         */
+        @Then("there is no best combination for max profit")
+        public void thereIsNoBestCombinationForMaxProfit() {
+                final CalculationResult actualResult = Stock.returnIndicesMaxProfit(
+                                savingsContext,
+                                currentPricesContext,
+                                futurePricesContext);
+                assertEquals(0, actualResult.indices.size(), "Actual Result: "
+                                + actualResult.indices);
+        }
+
+        /**
+         * Asserts that profit is 0.
+         *
+         */
+        @And("no profit is made")
+        public void noProfitIsMade() {
+                final int actualProfit = Stock.returnIndicesMaxProfit(savingsContext,
+                                currentPricesContext,
+                                futurePricesContext).maxProfit;
+                assertEquals(0, actualProfit, "Actual Profit is %s " + actualProfit);
+        }
+
+        /**
+         * Asserts that with the given indices, same profit is achieved as
+         * previously stored.
+         *
+         * @param indices combination representing the indices of stocks.
+         */
+        @And("same savings and max profit is achieved with the indices {string}")
+        public void sameSavingsMaxProfitIsWithCombination(final String indices) {
+                this.theBestCombinationOfIndicesForMaxProfitIs(indices);
+        }
 }
