2a39ca173f647830225057ac06f134f356c2939a
Dwane Debono <dwane.debono@gmail.com>
2023-02-21 10:04:04 +0000

    linting

--- src/main/java/com/maxprofit/calculator/CalculationResult.java
+++ src/main/java/com/maxprofit/calculator/CalculationResult.java
@@ -12,36 +12,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class CalculationResult {
-    /**
-     * The maximum profit obtained.
-     */
-    public int maxProfit;
 
-    /**
-     * A list of lists of indices representing the best combination of indices to
-     * obtain the maximum profit.
-     */
-    public List<List<Integer>> indices;
+  /**
+   * The maximum profit obtained.
+   */
+  public int maxProfit;
 
-    /**
-     * Constructs a new CalculationResult object with the given maximum profit and
-     * list of indices.
-     * 
-     * @param profit      the maximum profit obtained
-     * @param combination a list of lists of indices representing the best
-     *                    combination of indices to obtain the maximum profit
-     */
-    public CalculationResult(final int profit, final List<List<Integer>> combination) {
-        this.maxProfit = profit;
-        this.indices = combination;
-    }
+  /**
+   * A list of lists of indices representing the best combination of indices to
+   * obtain the maximum profit.
+   */
+  public List<List<Integer>> indices;
 
-    /**
-     * Constructs a new CalculationResult object with a maximum profit of 0 and an
-     * empty list of indices.
-     */
-    public CalculationResult() {
-        this.maxProfit = 0;
-        this.indices = Collections.emptyList();
-    }
+  /**
+   * Constructs a new CalculationResult object with the given maximum profit and
+   * list of indices.
+   *
+   * @param profit      the maximum profit obtained
+   * @param combination a list of lists of indices representing the best
+   *                    combination of indices to obtain the maximum profit
+   */
+  public CalculationResult(
+    final int profit,
+    final List<List<Integer>> combination
+  ) {
+    this.maxProfit = profit;
+    this.indices = combination;
+  }
+
+  /**
+   * Constructs a new CalculationResult object with a maximum profit of 0 and an
+   * empty list of indices.
+   */
+  public CalculationResult() {
+    this.maxProfit = 0;
+    this.indices = Collections.emptyList();
+  }
 }
