8b43b170fdc12eef7f4e23f4f9335d7f23991790
Dwane Debono <dwane.debono@gmail.com>
2023-03-24 21:44:49 +0200

    Adding Dependabot report in Workflow (#6)
    * Adding Dependabot report in Workflow
    
    * use latest version
    
    * Added OWASP dependency checker
    
    * switched mvn package to mvn site so that verify lifecycle checks dependencies
    
    * Adding dep check separately in actions
    
    * fix workflow
    
    * Update maven.yml
    
    Signed-off-by: Dwane Debono <dwane.debono@gmail.com>
    
    * [MegaLinter] Apply linters fixes
    
    * [MegaLinter] Apply linters fixes
    
    * [MegaLinter] Apply linters fixes
    
    * triggering action
    
    * Attempting fix for permissions error in checkov linter
    
    * Attempting fix for permissions error in checkov linter
    
    * Attempting fix for permissions error in checkov linter
    
    * Attempting fix for permissions error in checkov linter
    
    * Adding write permission to Junit report
    
    * Adding write permission to Junit report
    
    * Adding write permission to Junit report
    
    * dependency upgrades due to CVEs
    
    * Logger changes
    
    * Adding clean lifecycle
    
    * Moved to Java 17 due to Spring 6
    
    * Dependencies update
    
    * Dependencies update
    
    * Multiple changes to fix dependency-check
    
    * Linting fix
    
    * Linting fix
    
    * Linting fix
    
    * permissions
    
    * permissions
    
    * permissions
    
    * permissions
    
    * permissions
    
    * top-level permissions
    
    * top-level permissions
    
    * Removed megalinter-report directory
    
    * [MegaLinter] Apply linters fixes
    
    * Fail only on critical vulnerability
    
    ---------
    
    Signed-off-by: Dwane Debono <dwane.debono@gmail.com>
    Co-authored-by: dwaned <dwaned@users.noreply.github.com>

--- src/test/java/com/maxprofit/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/com/maxprofit/calculator/controller/CalculatorControllerTest.java
@@ -1,7 +1,6 @@
 package com.maxprofit.calculator.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.maxprofit.calculator.CalculationResult;
 import com.maxprofit.calculator.Controller.CalculationRequest;
 import com.maxprofit.calculator.Controller.CalculatorController;
 import org.junit.jupiter.api.Test;
@@ -15,17 +14,32 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * This class contains unit tests for the {@link CalculatorController} class.
+ */
+@SuppressWarnings({"checkstyle:LineLength", "checkstyle:magicnumber"})
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
+    /**
+     * The {@link MockMvc} instance that is used to perform HTTP requests against.
+     */
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * The {@link ObjectMapper} instance that is used to serialize and deserialize.
+     */
     @Autowired
     private ObjectMapper objectMapper;
 
+    /**
+     * @throws Exception if an error occurs during the test
+     */
     @Test
     public void testCalculate() throws Exception {
         CalculationRequest request = new CalculationRequest();
@@ -34,7 +48,7 @@ public class CalculatorControllerTest {
         request.setFuturePrices(Arrays.asList(15, 10, 35));
         String jsonRequest = objectMapper.writeValueAsString(request);
 
-        CalculationResult expected = new CalculationResult();
+
 
         mockMvc.perform(post("/api/calculate")
                         .contentType(MediaType.APPLICATION_JSON)
