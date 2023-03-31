0bfcfa6218451d096a7cbcaf3914f58d00c88dbc
Dwane <dwane.debono@gmail.com>
2023-03-31 09:50:38 +0300

    Megalinter fixes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM openjdk:17-jdk-alpine
+
+RUN addgroup -S appgroup && adduser -S appuser -G appgroup
+USER appuser
+
 VOLUME /tmp
 ARG JAR_FILE=target/max-profit-calculator-1.0-SNAPSHOT-jar-with-dependencies.jar
 WORKDIR /app
--- src/main/java/com/maxprofit/calculator/MyApplication.java
+++ src/main/java/com/maxprofit/calculator/MyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyApplication {
+public final class MyApplication {
 
     /**
      * @param args the command line arguments
