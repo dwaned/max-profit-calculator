5afaed2a357b01cf95026ad5c63c8c9aa0c97464
Dwane <dwane.debono@gmail.com>
2023-05-10 09:36:31 +0300

    Moving context path to application.properties

--- src/main/java/com/maxprofit/calculator/Controller/CalculatorController.java
+++ src/main/java/com/maxprofit/calculator/Controller/CalculatorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("checkstyle:LineLength")
 @RestController
-@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:9095")
 public class CalculatorController {
 
     /**
